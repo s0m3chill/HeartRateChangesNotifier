1cf210543dcd25a42a04feaf6be71dbf47a93855
Dariy <d.kordiyak@gmail.com>
2023-02-03 23:06:32 +0200

    Create project

---
+++ HeartRateChangesNotifier/HeartRateChangesNotifier/HeartRateChangesNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateChangesNotifierApp.swift
+//  HeartRateChangesNotifier
+//
+//  Created by Dariy Kordiyak on 03.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateChangesNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
