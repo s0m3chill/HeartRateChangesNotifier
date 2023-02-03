dca83a19c2851c6e14fdf89be9f1c86b872e90b3
Dariy <d.kordiyak@gmail.com>
2023-02-03 23:11:38 +0200

    Add HeartRateStatus

---
+++ HeartRateChangesNotifier/HeartRateChangesNotifier/HeartRateStatus.swift
@@ -0,0 +1,13 @@
+//
+//  HeartRateStatus.swift
+//  HeartRateChangesNotifier
+//
+//  Created by Dariy Kordiyak on 03.02.2023.
+//
+
+import Foundation
+
+enum HeartRateStatus {
+    case stimulation
+    case measurement
+}
