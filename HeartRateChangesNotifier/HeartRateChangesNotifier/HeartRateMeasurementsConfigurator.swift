236be104fc4c395b81f9ebc3c3ac5bff9a800d79
Dariy <d.kordiyak@gmail.com>
2023-02-04 09:02:54 +0200

    Add HRMeasurementsConfigurator

---
+++ HeartRateChangesNotifier/HeartRateChangesNotifier/HeartRateMeasurementsConfigurator.swift
@@ -0,0 +1,16 @@
+//
+//  HeartRateMeasurementsConfigurator.swift
+//  HeartRateChangesNotifier
+//
+//  Created by Dariy Kordiyak on 04.02.2023.
+//
+
+import Foundation
+
+class HeartRateMeasurementsConfigurator {
+    
+    var status: HeartRateStatus!
+    
+    
+    
+}
